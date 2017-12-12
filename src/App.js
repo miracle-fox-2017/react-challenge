d9f228bdd34a8680cfe12e718e0816ef95ac669a
Septian Ahmad Fujianto <septianahmad@naisinpo.com>
2017-12-12 11:54:50 +0700

    Add minor styling

--- src/App.js
+++ src/App.js
@@ -1,31 +1,70 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+var html = require('react-escape-html');
+var striptags = require('striptags');
 
 class App extends Component {
   constructor (props) {
     super(props)
 
     this.state = {
-      cards: [{title: 'exodia'}]
+      articles: [],
     }
   }
 
-  render() {
+  componentWillMount () {
+    const apiUrl = 'http://www.sfexaminer.com/wp-json/wp/v2/posts/?_embed'
+
+    axios.get(apiUrl)
+      .then(({data}) => {
+        this.setState({
+          articles : data
+        })
+
+      }).catch(err => console.error({ message: 'Something wrong', error: err.message }));
+  }
+
+  render () {
+    const articleItemStyle = {
+      border: '1px solid #ddd',
+      margin: '20px',
+      padding: '10px',
+      width: '50%',
+      background: 'ghostwhite',
+    };
+
+    const featuredImageStyle = {
+      width: '100%' 
+    }
+
+    const articleListStyle = {
+      display: 'flex',
+      flexWrap: 'wrap',
+      justifyContent: 'center'
+    }
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">React Duelist</h1>
+          <h1 className="App-title">React News</h1>
         </header>
 
-        <div className="cardList">
+        <div className="articleList" style={articleListStyle}>
           {
-            this.state.cards.map((card) => {
+            this.state.articles.map((article, index) => {
               return (
-                <div className = "cardItem" >
-                  <img src="" alt="Exodia"/>
-                  <h3 className="cardTitle">Exodia</h3>
+                <div className = "articleItem" key={index} style={articleItemStyle}>
+                  <h2 className="articleTitle">{article.title.rendered}</h2>
+            
+                  <img style={featuredImageStyle} src = {
+                      article._embedded !== null && typeof article._embedded['wp:featuredmedia'] !== 'undefined' ? article._embedded['wp:featuredmedia'][0].source_url : 'https://placeimg.com/300/200/animal'}/>
+                  
+                  <br/>
+
+                  { striptags(article.excerpt.rendered) }
                 </div>
               )
             })
