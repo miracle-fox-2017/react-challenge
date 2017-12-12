29900ec5d96323955e7217a4778288e834bd6f88
vbagustinus <vbagustinus@gmail.com>
2017-12-12 21:50:25 +0700

    First Routing

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Home from './Home'
+import Inputs from './Input'
 import './App.css';
 import firebase from 'firebase';
-import Axios from 'axios'
-import { Layout, Form, Input, Button, Card } from 'antd';
-const FormItem = Form.Item;
-const { Header, Content, Footer } = Layout;
-
-const formItemLayout = {
-  labelCol: { span: 4 },
-  wrapperCol: { span: 8 },
-};
-
-const gridStyle = {
-  width: '25%',
-  textAlign: 'center',
-};
+import { Layout, Menu } from 'antd';
+const { Header, Content } = Layout;
 
 class App extends Component {
   constructor(){
@@ -37,127 +27,35 @@ class App extends Component {
       }
     };
   }
-
-  componentWillMount  = () => {
-    let getData = firebase.database().ref('muka')
-    let that  = this
-    getData.on('value', function(snapshot) {
-      let tampung = []
-      snapshot.forEach(function(childSnapshot) {
-        var childData = childSnapshot.val();
-        tampung.push(childData)
-      });
-      that.setState({
-        allPhotos: tampung
-      })       
-    })
-  }
-
-  onChanges = (e) => {
-    let state = this.state.newData
-    state[e.target.name]= e.target.value
-    this.setState(state)
-  }
-  Processing = (e) => {
-    e.preventDefault()
-    console.log(this.state.avatarURL)
-    Axios.post(`http://api.skybiometry.com/fc/faces/detect.json?api_key=6obnuu0u9u04bvimnne28ni345&api_secret=5c9bq4igc78d0islt74i7fi2qa&urls=${this.state.avatarURL}&attributes=all`)
-    .then(({data}) => {
-      let result = data.photos[0].tags[0].attributes
-      let db = firebase.database().ref('muka')
-      console.log(db)
-      db.push({
-        jenis_kelamin : result.gender.value,
-        umur : result.age_est.value,
-        senang : result.happiness.value,
-        perasaan : result.mood.value,
-        tersenyum : result.smiling.value,
-        mata : result.eyes.value,
-        marah : result.anger.value,
-        ketakutan : result.fear.value,
-        kesedihan : result.sadness.value,
-        perasaan_baik : result.neutral_mood.value,
-        bibir : result.lips.value,
-        kaca_mata : result.glasses.value,
-        url: this.state.newData.avatarURL
-      })
-      this.setState({
-        newData: {
-          avatarURL: ''
-        }
-      })
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-  }
-
   render() {
     return (
       <div className="App">
-      <Layout>
-        <Header><img src={logo} className="App-logo" alt="logo" />       </Header>
-        <Layout>
-          <Content>
-          <Form>
-            <br/>
-              <label>Foto:</label>
-              <br/>
-              <FormItem {...formItemLayout}>
-                <Input name="avatarURL" value={this.state.newData.avatarURL} placeholder="Link url  gambars" onChange={this.onChanges} />
-              </FormItem>
-              <br/>
-              
-              {this.state.newData.avatarURL &&
-              <Card
-                hoverable
-                style={{ width: 240 }}
-                cover={<img alt="your  gambar" src={this.state.newData.avatarURL} />}
-                >
-              </Card>
-              }
-              <Button type="primary" onClick={this.Processing}>Process</Button>
-          </Form> 
-          <h1>All Photos</h1>
+      <Layout className="layout">
+        <Header>
+          <div className="logo" label="MukaMu"/>
+          <Router>
           <div>
-            <Card title=''>
-              {this.state.allPhotos.map(photos => {
-                return(  
-                  <Card.Grid style={gridStyle}>
-                    <img alt="" style={gambarStyle} src={photos.url} />
-                    <p>Jenis Kelamin: {photos.jenis_kelamin}</p>
-                    <p>Kaca Mata: {photos.kaca_mata}</p>
-                    <p>Kesedihan: {photos.kesedihan}</p>
-                    <p>Ketakutan: {photos.ketakutan}</p>
-                    <p>Marah: {photos.marah}</p>
-                    <p>Mata: {photos.mata}</p>
-                    <p>Perasaan: {photos.perasaan}</p>
-                    <p>Perasaan Baik: {photos.perasaan_baik}</p>
-                    <p>Senang: {photos.senang}</p>
-                    <p>Tersenyum: {photos.tersenyum}</p>
-                    <p>Umur: {photos.umur}</p>
-                    <p>Bibir: {photos.bibir}</p>
-                  </Card.Grid>
-                )
-              })}
-            </Card>
-          </div>
-          </Content>
-        </Layout>
-        <Footer>
-         
-        </Footer>
-      </Layout>
-        
-        
+            <Menu
+              theme="dark"
+              mode="horizontal"
+              style={{ lineHeight: '64px' }}
+            >
+              <Menu.Item><Link to="/">Home</Link></Menu.Item>
+              <Menu.Item><Link to="/input">Input</Link></Menu.Item>
+            </Menu>
+            <hr/>
+                <Route exact path="/" component={Home}/>
+                <Route path="/input" component={Inputs}/>
+              </div>
+          </Router>
+        </Header>
+        <Content style={{ padding: '0 50px' }}>
+          
+        </Content>
+      </Layout> 
       </div>
     );
   }
 }
-
-let  gambarStyle = {
-  width: "auto",
-  height: "200px"
-};
 
 export default App;
